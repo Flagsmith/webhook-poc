43859406167ec92d4721ac70e8adc770e2ea703c
kyle-ssg <kyle@solidstategroup.com>
2022-05-08 08:54:53 +0100

    Update webhook

--- pages/api/flagsmith-webhook.ts
+++ pages/api/flagsmith-webhook.ts
@@ -9,9 +9,6 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    console.log(Object.keys(req.body))
-    console.log(req.body.action, "ACTION")
-    console.log(req.body.changes, "CHANGES")
-    console.log(req.body.comment, "COMMENT")
+    console.log(req.body)
     res.status(200).json({ name: 'John Doe' })
 }
