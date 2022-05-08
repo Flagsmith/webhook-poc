e53903d953a447c1b928f75ddd09ea6df1b16938
kyle-ssg <kyle@solidstategroup.com>
2022-05-08 12:53:01 +0100

    log sender

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -207,6 +207,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const body:Data = req.body;
+  console.log(body.sender)
   // await createComment(body.repository.owner.login, body.repository.name, body.issue.number, "Test")
   res.json({ok:1})
 
