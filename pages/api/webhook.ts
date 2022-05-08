4b4106db4a9880aeee255c5cfd861de408ccf2ad
kyle-ssg <kyle@solidstategroup.com>
2022-05-08 14:30:38 +0100

    Update types

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -200,6 +200,37 @@ export interface Data {
     type: string;
     site_admin: boolean;
   };
+  comment:{
+    url: string;
+    html_url: string;
+    issue_url: string;
+    id: number;
+    node_id: string;
+    user: {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+    };
+    created_at: Date;
+    updated_at: Date;
+    author_association: string;
+    body: string;
+  }
 }
 
 export default async function handler(
@@ -207,9 +238,10 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const body:Data = req.body;
-  console.log(Object.keys(req.body))
-  console.log(req.body.comment, "comment")
-  console.log(req.body.sender, "sender")
+  if (body.sender.type === 'User') {
+    // set feature states based on comment
+  }
   // console.log(req.body.organization, "organization")
   res.status(200).json({ name: 'John Doe' })
 }
+
