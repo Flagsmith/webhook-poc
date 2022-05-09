0505e98e4e59dbfdb349b9be809139052525ce9d
kyle-ssg <kyle@solidstategroup.com>
2022-05-09 20:43:53 +0100

    log associated flag

--- pages/api/flagsmith-webhook.ts
+++ pages/api/flagsmith-webhook.ts
@@ -60,7 +60,7 @@ type Data =  {
 }
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse
 ) {
     const body:Data = req.body;
     const associatedFlag = body.data.new_state.feature.id;
