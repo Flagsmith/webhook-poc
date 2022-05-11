c55f19f0dd59b133d2a7b8bd37090ec9f8c00de1
kyle-ssg <kyle@solidstategroup.com>
2022-05-11 21:44:21 +0100

    parse language

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,34 +6,34 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
-const comment = `Flagsmith Feature:
-**Production**
-- [ ] Environment Default
-
-**Development**
-- [x] Environment Default 
-\`\`\`xml
-<div/>
-\`\`\`
-
-- [ ] internal_segment
-
-
-Last Updated 11th May 2022 19:44pm
-`
-
-const [initial,...rest] = comment.split(/\*\*(.*?)\*\*/g)
-let environments = {
-
-}
-let currentEnvironment = ""
-rest.forEach((v)=>{
-  if (!v.startsWith('\n')) {
-    environments[v] = {}
-    currentEnvironment = v
-  } else {
-    const featureValues = v.split
-    environments[currentEnvironment] = v
-  }
-})
-console.log(environments)
+// const comment = `Flagsmith Feature:
+// **Production**
+// - [ ] Environment Default
+//
+// **Development**
+// - [x] Environment Default
+// \`\`\`xml
+// <div/>
+// \`\`\`
+//
+// - [ ] internal_segment
+//
+//
+// Last Updated 11th May 2022 19:44pm
+// `
+//
+// const [initial,...rest] = comment.split(/\*\*(.*?)\*\*/g)
+// let environments = {
+//
+// }
+// let currentEnvironment = ""
+// rest.forEach((v)=>{
+//   if (!v.startsWith('\n')) {
+//     environments[v] = {}
+//     currentEnvironment = v
+//   } else {
+//     const featureValues = v.split
+//     environments[currentEnvironment] = v
+//   }
+// })
+// console.log(environments)
