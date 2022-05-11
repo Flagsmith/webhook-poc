bee8a06ba779ce3f016d09891db2255d868ff0b2
kyle-ssg <kyle@solidstategroup.com>
2022-05-11 21:39:12 +0100

    adjust feature comment

--- utils/github/create-comment-text.ts
+++ utils/github/create-comment-text.ts
@@ -1,49 +1,21 @@
-import { IFeaturesResults} from "../flagsmith/fetch-feature";
+import {IFeaturesResults} from "../flagsmith/fetch-feature";
 import moment from 'moment'
-import xml from "fast-xml-parser";
-import toml from 'toml'
-import yaml from 'yaml'
+import parseLanguage from "../parseLanguage";
 
 export default function (data:IFeaturesResults[]) {
     return `Flagsmith Feature:
 ${data.map((featureResults)=>{
         return `${featureResults.features.map((v)=>{
             let featureValue = v.feature_state_value.integer_value || v.feature_state_value.string_value || v.feature_state_value.boolean_value
-            let language = ''
-                try {
-                    const x = xml.XMLValidator.validate(featureValue)
-                    if (x!== true) {
-                        throw new Error("error")
-                    }
-                    language = 'xml'
-                } catch (e) {
-                        try {
-                            toml.parse(featureValue)
-                            language = 'ini'
-                        } catch (e) {
-                            try {
-                                JSON.parse(featureValue)
-                                language = 'json'
-                            } catch (e) {
-                                try {
-                                    yaml.parse(featureValue)
-                                    language = 'yaml'
-                                } catch (e) {
-                                
-                                }
-                            }
-                        }
-                }
-            
-            
-            const hasFeature = featureValue!=null && typeof featureValue!='undefined'
-            return `**${featureResults.environment.name}${v.segment?.name?` ${v.segment.name}`:""}**
-            - [${v.enabled?'x':' '}] Enabled${hasFeature?
-`
+            const hasFeatureValue = featureValue!=null && typeof featureValue!='undefined'
+            let language = hasFeatureValue ? parseLanguage(featureValue) : ''
+            const featureValueString = hasFeatureValue? `
 \`\`\`${language}
 ${featureValue}
 \`\`\`
-`:''}`
+`:''
+            return `**${featureResults.environment.name}${v.segment?.name?` ${v.segment.name}`:""}**
+            - [${v.enabled?'x':' '}] Enabled${featureValueString}`
 }).join("\n")}
 `}).join("\n")}
 
