616f9995fc7c60307051d12b337e29016cb7a5f2
kyle-ssg <kyle@solidstategroup.com>
2022-05-15 16:10:07 +0100

    Update no value text

--- utils/mockedConstants.ts
+++ utils/mockedConstants.ts
@@ -2,7 +2,7 @@ export default {
     flag: '22182',
     githubComment:1120416111,
     githubIssue:1067,
-    featureNoValue:'!No Value',
+    featureNoValue:'No Value.',
     githubOwner:'Flagsmith',
     githubRepo:'flagsmith',
     project:'7087',
