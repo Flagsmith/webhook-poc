fd5c656fc8c73c4c97f5491f8b91b2a4c0cb82b8
Novak Zaballa <zaballa.novak@gmail.com>
2024-02-16 15:14:57 -0400

    Delete some comments

--- pages/api/repositories.ts
+++ pages/api/repositories.ts
@@ -1,21 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
 
 import fetchRepositories from '../../utils/github/api/fetch-repositories';
-import Cors from 'cors';
-
-// const cors = Cors({
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   });
-  
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    // await cors(req, res);
-    // req.headers['ngrok-skip-browser-warning'] = true;
+
     const body:Data = req.query
     const installationId = body?.installation_id
     const resGetRepos = await fetchRepositories(installationId)
--- utils/github/api/github-client.ts
+++ utils/github/api/github-client.ts
@@ -5,12 +5,14 @@ let _token:string;
 //authorises with GitHub
 export default async function (installationId:string) {
     if(_token) return _token
+    console.log("DEBUG: process.env.GITHUB_PEM:", process.env.GITHUB_PEM)
+    console.log("DEBUG: installationId:", installationId)
     const auth = createAppAuth({
         appId: process.env.GITHUB_APP_ID,
-        privateKey: process.env.GITHUB_PEM.split(String.raw`\n`).join('\n'),
         installationId,
         clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        privateKey: process.env.GITHUB_PEM,
     });
     const { token } = await auth({ type: 'installation' });
     _token = token
